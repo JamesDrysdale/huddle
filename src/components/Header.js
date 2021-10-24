f90038e95af0801b50c02ec3ddaa9bf0de904293
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-10-24 16:04:38 +0100

    App has a basic red header placeholder

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+import ( StyledHeader)
+
+export default function Header () {
+    return (
+        <StyledHeader bg='red'>
+            <h1>Hubble</h1>
+        </StyledHeader>
+    )
+}
---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    background-color: ${( { bg } ) => bg};
+    padding: 40px 0;
+
+`

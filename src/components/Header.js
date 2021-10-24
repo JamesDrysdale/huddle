a095524f9ee43411ef1b17763102f82d32586fca
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-10-24 19:33:34 +0100

    Header contains company logo and call to action button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
-import { StyledHeader } from "./styles/Header.styled";
-
-import ( StyledHeader)
+import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
 
 export default function Header () {
     return (
         <StyledHeader>
-            <h1>Hubble</h1>
+            <Container>
+                <Nav>
+                    <Logo src='./images/logo.svg' alt='' />
+                    <Button>Try It Free</Button>
+                </Nav>
+            </Container>
         </StyledHeader>
     )
 }
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -5,3 +5,12 @@ export const StyledHeader = styled.header`
     padding: 40px 0;
 
 `
+
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 40px;
+`
+
+export const Logo = styled.img``
